e8ac2b0a64678159aa7ad0613c5f58725bfb1185
Euna Ahn <eunaahn@naver.com>
2023-11-09 11:10:22 +0900

    Title: 배열의 특정 위치 찾기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231109/배열의 특정 위치 찾기/find-specific-location-fo-array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    int sum_1 = 0;
+    double sum_2 = 0;
+    double cnt = 0;
+
+    // 여기에 코드를 작성해주세요.
+    for(int i=0; i<10; i++){
+        cin >> arr[i];
+        if(i%2!=0){
+            sum_1 += arr[i];
+        }
+        if(i%2==0 && i!=0){
+            sum_2 += arr[i];
+            cnt++;
+        }
+    }
+
+    cout << fixed;
+    cout.precision(1);
+    cout << sum_1 << " " << sum_2/cnt;
+
+    return 0;
+}
