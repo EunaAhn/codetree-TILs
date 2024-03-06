2caad774d2062b126b2b276bab98d863e1d030ac
Euna Ahn <eunaahn@naver.com>
2024-03-06 13:38:21 +0900

    Title: 나눗셈의 나머지, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240306/나눗셈의 나머지/remainder-of-division.java
+++ 240306/나눗셈의 나머지/remainder-of-division.java
@@ -6,19 +6,19 @@ public class Main {
 
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int[] count = new int[b]; // b까지의 나머지 개수를 저장할 배열
+        int[] count = new int[10]; // b까지의 나머지 개수를 저장할 배열
+        int ans = 0;
 
         while (a > 1) {
-            int remainder = a % b;
-            count[remainder]++; // 나머지 개수 증가
+            count[a%b]++; // 나머지 개수 증가
             a /= b; // a를 b로 나눈 몫으로 업데이트
         }
 
-        int sum = 0;
+        // b로 나눈 나머지 값만 계산에 이용되기 때문에 0부터 b-1까지만 포문을 돌려주면됨 
         for (int i = 0; i < b; i++) {
-            sum += count[i] * count[i]; // 나머지 개수의 제곱을 합산
+            ans += count[i] * count[i]; // 나머지 개수의 제곱을 합산
         }
 
-        System.out.println(sum);
+        System.out.println(ans);
     }
 }
